f8ff2e16670ada95857bcd19f8bfc3f63b9fa489
Edward Winata <edwardwinata@gmail.com>
2022-05-27 16:59:14 +0700

    enabled CORS & pages for GET and POST requests

--- React/src/pages/CreatePost.jsx
+++ React/src/pages/CreatePost.jsx
@@ -1,9 +1,51 @@
-import React, { useState, useEffect } from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [name, setName] = useState('');
+  const [alias, setAlias] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const hero = { name, alias };
+
+    setIsPending(true);
+
+    fetch('http://127.0.0.1:8000/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(hero)
+    }).then(() => {
+      console.log('New hero added');
+      setIsPending(false);
+      navigate('/');
+    })
+  }
+
   return (
     <div>
-      CreatePost
+      <h2>Add a superhero!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          required
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <label>Alias:</label>
+        <input
+          type="text"
+          required
+          value={alias}
+          onChange={(event) => setAlias(event.target.value)}
+        />
+        {!isPending && <button> Confirm </button>}
+        {isPending && <button disabled> Loading </button>}
+      </form>
     </div>
   );
 }
